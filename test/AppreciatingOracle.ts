b642e6d1c828ae7f7bc0b3eb2cdaaf3af3eb43a2
Yash Agrawal <yashagrawal7098@gmail.com>
2022-06-14 14:05:46 +0530

    feat(test): add some more unit tests

--- test/AppreciatingOracle.ts
+++ test/AppreciatingOracle.ts
@@ -28,7 +28,7 @@ describe("Appreciating Oracle", () => {
     AppreciatingOracle = await ethers.getContractFactory("AppreciatingOracle");
   });
 
-  before("Deploy contracts", async () => {
+  beforeEach("Deploy contracts", async () => {
     mockChainlinkAggregator = await MockChainlinkAggregator.connect(
       operator
     ).deploy();
@@ -48,6 +48,24 @@ describe("Appreciating Oracle", () => {
     });
 
     it("Should fetch chainlink feed price correctly", async () => {
+      expect(await appreciatingOracle.currFeedPrice()).to.eq(
+        GMU_PRECISION.mul(2)
+      );
+    });
+  });
+
+  describe("Fetch the price when market is down after start but price update is not called", async () => {
+    beforeEach("Set chainlink oracle price to 1.5", async () => {
+      await mockChainlinkAggregator.setPrice(
+        CHAINLINK_PRECISION.mul(15).div(10) // 1.5 times CHAINLINK_PRECISION
+      );
+    });
+
+    it("Should fetch starting GMU price correctly", async () => {
+      expect(await appreciatingOracle.currPrice()).to.eq(GMU_PRECISION);
+    });
+
+    it("Should fetch starting chainlink feed price correctly", async () => {
       expect(await appreciatingOracle.currFeedPrice()).to.eq(
         GMU_PRECISION.mul(2)
       );
