ae6448438c989ac96330c353efbe215f5af224d8
Steven Enamakel <enamakel@mahadao.com>
2022-07-03 21:21:48 +0530

    added linear appreciating oracle

---
+++ contracts/LinearAppreciatingOracle.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {AggregatorV3Interface} from "./interfaces/AggregatorV3Interface.sol";
+import {IPriceFeed} from "./interfaces/IPriceFeed.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * An appreciating oracle that linearly appreciates a value over time.
+ *
+ * @author Steven Enamakel enamakel@mahadao.com
+ */
+contract LinearAppreciatingOracle is IPriceFeed, Ownable {
+    using SafeMath for uint256;
+
+    string public name;
+    uint256 public startPrice;
+    uint256 public endPrice;
+    uint256 public endPriceTime;
+    uint256 public startPriceTime;
+    uint256 public targetDigits = 18;
+
+    uint256 internal _priceDiff;
+    uint256 internal _timeDiff;
+
+    constructor(string memory _name, uint256 _startingPrice) {
+        name = _name;
+        startPrice = _startingPrice;
+        endPrice = _startingPrice;
+        endPriceTime = block.timestamp;
+        startPriceTime = block.timestamp;
+    }
+
+    function notifyNewPrice(uint256 newPrice, uint256 extraTime)
+        external
+        onlyOwner
+    {
+        require(extraTime > 0, "bad time");
+
+        startPrice = _fetchPriceAt(block.timestamp);
+        require(newPrice > startPrice, "bad price");
+
+        endPrice = newPrice;
+        endPriceTime = block.timestamp + extraTime;
+        startPriceTime = block.timestamp;
+
+        _priceDiff = endPrice.sub(startPrice);
+        _timeDiff = endPriceTime.sub(startPriceTime);
+    }
+
+    function fetchPrice() external view override returns (uint256) {
+        return _fetchPriceAt(block.timestamp);
+    }
+
+    function fetchPriceAt(uint256 time) external view returns (uint256) {
+        return _fetchPriceAt(time);
+    }
+
+    function _fetchPriceAt(uint256 time) internal view returns (uint256) {
+        if (startPriceTime >= time) return startPrice;
+        if (endPriceTime <= time) return endPrice;
+
+        uint256 percentage = (time.sub(startPriceTime)).mul(1e24).div(
+            _timeDiff
+        );
+
+        return startPrice + _priceDiff.mul(percentage).div(1e24);
+    }
+
+    function getDecimalPercision() external view override returns (uint256) {
+        return targetDigits;
+    }
+
+    function timestamp() external view returns (uint256) {
+        // for testing purposes
+        return block.timestamp;
+    }
+}
---
+++ test/LinearAppreciatingOracle.ts
@@ -0,0 +1,114 @@
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe.only("LinearAppreciatingOracle", async function () {
+  const { provider } = ethers;
+  const one = BigNumber.from(10).pow(18);
+
+  let oracle: Contract;
+
+  beforeEach(async function () {
+    const LinearAppreciatingOracle = await ethers.getContractFactory(
+      "LinearAppreciatingOracle"
+    );
+
+    oracle = await LinearAppreciatingOracle.deploy(
+      "Test LinearAppreciatingOracle",
+      one
+    );
+  });
+
+  it("Should deploy properly", async function () {
+    expect(await oracle.fetchPrice()).to.be.equal(one);
+    expect(await oracle.startPrice()).to.be.equal(one);
+    expect(await oracle.endPrice()).to.be.equal(one);
+  });
+
+  describe("notify new price once for a duration of 1 day ", async function () {
+    let now: number;
+    let startPrice: number;
+
+    beforeEach(async function () {
+      expect(await oracle.fetchPrice()).to.be.equal(one);
+      now = Number(await oracle.timestamp());
+
+      await oracle.notifyNewPrice(one.mul(2), 86400);
+
+      now = Number(await oracle.timestamp());
+      startPrice = await oracle.startPrice();
+      expect(await oracle.endPrice()).to.be.equal(one.mul(2));
+    });
+
+    it("Should report price properly at hour 0", async function () {
+      expect(await oracle.fetchPriceAt(now)).to.be.equal(startPrice);
+    });
+
+    it("Should report price properly at hour 6", async function () {
+      expect(await oracle.fetchPriceAt(now + 21600)).to.be.equal(
+        one.mul(125).div(100)
+      );
+    });
+
+    it("Should report price properly at hour 12", async function () {
+      expect(await oracle.fetchPriceAt(now + 43200)).to.be.equal(
+        one.mul(15).div(10)
+      );
+    });
+
+    it("Should report price properly at hour 24", async function () {
+      expect(await oracle.fetchPriceAt(now + 86400)).to.be.equal(one.mul(2));
+    });
+
+    it("Should report price properly at hour 48", async function () {
+      expect(await oracle.fetchPriceAt(now + 172800)).to.be.equal(one.mul(2));
+    });
+
+    describe("notify new price again after 12 hours in the same day for a duration of 1 day ", async function () {
+      beforeEach(async function () {
+        await provider.send("evm_increaseTime", [43200]);
+        await oracle.notifyNewPrice(one.mul(4), 86400);
+
+        now = Number(await oracle.timestamp());
+        startPrice = await oracle.startPrice();
+
+        expect(await oracle.endPriceTime()).to.be.equal(now + 86400);
+        expect(await oracle.endPrice()).to.be.equal(one.mul(4));
+      });
+
+      it("Should report price properly at hour 12", async function () {
+        expect(await oracle.fetchPriceAt(now)).to.be.equal(startPrice);
+      });
+
+      it("Should report price properly at hour 18", async function () {
+        expect(
+          (await oracle.fetchPriceAt(now + 21600)).mul(100).div(one)
+        ).to.be.equal(212);
+      });
+
+      it("Should report price properly at hour 24", async function () {
+        expect(
+          (await oracle.fetchPriceAt(now + 43200)).mul(100).div(one)
+        ).to.be.equal(275);
+      });
+
+      it("Should report price properly at hour 36", async function () {
+        expect(
+          (await oracle.fetchPriceAt(now + 64800)).mul(100).div(one)
+        ).to.be.equal(337);
+      });
+
+      it("Should report price properly at hour 48", async function () {
+        expect(
+          (await oracle.fetchPriceAt(now + 172800)).mul(100).div(one)
+        ).to.be.equal(400);
+      });
+
+      it("Should report price properly at hour 60", async function () {
+        expect(
+          (await oracle.fetchPriceAt(now + 216000)).mul(100).div(one)
+        ).to.be.equal(400);
+      });
+    });
+  });
+});
