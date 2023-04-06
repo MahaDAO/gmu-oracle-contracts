121e72bac437728311b634dd11ad085a3f84806e
Steven Enamakel <enamakel@mahadao.com>
2023-04-06 23:08:57 +0300

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
+
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-abi-exporter";
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ dotenv.config();
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
+
   for (const account of accounts) {
     console.log(account.address);
   }
@@ -21,41 +24,77 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+const config: HardhatUserConfig & any = {
+  vyper: {
+    version: "0.2.7",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        // blockNumber: 16334756,
+        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_ARB}`,
       },
       accounts: [
         {
           balance: "100000000000000000000",
-          privateKey:
-            process.env.PRIVATE_KEY !== undefined
-              ? process.env.PRIVATE_KEY
-              : "",
+          privateKey: process.env.PRIVATE_KEY,
         },
       ],
     },
+    arbitrum: {
+      url: `https://arb-mainnet.g.alchemy.com/v2/Pm5HPy74LXdxdHPfFOYlRaVaxgL_SE84`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     ethereum: {
       url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      // `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      gasPrice: 14000000000,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    bsc: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    polygon: {
-      url: "https://polygon-rpc.com/",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+  },
+  abiExporter: {
+    path: "./output/abis/",
+    runOnCompile: false,
+    clear: true,
+    flat: true,
+    spacing: 2,
+    pretty: true,
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
@@ -63,13 +102,11 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      polygon: process.env.POLYGONSCAN_API_KEY,
-      bsc: process.env.BSCSCAN_API_KEY,
       mainnet: process.env.ETHERSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      arbitrumOne: process.env.ARBISCAN_API_KEY,
     },
-  },
-  mocha: {
-    timeout: 100000000,
   },
 };
 
